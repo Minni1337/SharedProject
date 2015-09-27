13bd4b180244c35cf083c68d1bdce1496b89b4b7
Minni <minni1337@gmail.com>
2015-09-27 11:28:40 +0200

    Added alot of shit

--- BadBoys2/Global.asax.cs
+++ BadBoys2/Global.asax.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace BadBoys2
-{
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
+namespace BadBoys2 {
+  public class MvcApplication : System.Web.HttpApplication {
+    protected void Application_Start() {
+      AreaRegistration.RegisterAllAreas();
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
     }
+  }
 }
